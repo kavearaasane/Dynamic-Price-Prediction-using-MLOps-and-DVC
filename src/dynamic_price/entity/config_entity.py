1208e79d73468623e3c451fccf08f14274709d14
kavearaasane <109030811+kavearaasane@users.noreply.github.com>
2023-12-27 16:48:03 +0530

    Data Validation Pipleline Added

--- src/dynamic_price/entity/config_entity.py
+++ src/dynamic_price/entity/config_entity.py
@@ -1,10 +1,18 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-
+#Data Ingestion
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+# Data Validation
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
