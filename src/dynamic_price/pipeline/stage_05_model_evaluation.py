a371c27bb25987fcfd324d8d6e5049163f04267d
kavearaasane <109030811+kavearaasane@users.noreply.github.com>
2023-12-31 21:01:58 +0530

    Model Evaluation Pipeline Added

--- src/dynamic_price/pipeline/stage_05_model_evaluation.py
+++ src/dynamic_price/pipeline/stage_05_model_evaluation.py
@@ -13,7 +13,7 @@ class ModelEvaluationTrainingPipeline:
         config = ConfigurationManager()
         model_evaluation_config = config.get_model_evaluation_config()
         model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
-        #model_evaluation_config.log_into_mlflow()
+        model_evaluation_config.log_into_mlflow()
 
 
 
